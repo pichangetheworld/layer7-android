5d7dc12b8a2a4c6e2fa8c849e0b29d63d3bfd75d
pchan <pichangetheworld@gmail.com>
2015-08-31 22:21:35 +0900

    Adding LocationServices API. App defaults to user's location on the map.

--- app/src/main/java/layer7/layer7/MainActivity.java
+++ app/src/main/java/layer7/layer7/MainActivity.java
@@ -1,5 +1,6 @@
 package layer7.layer7;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,9 +9,14 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.common.collect.Lists;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -22,10 +28,17 @@ import org.json.JSONObject;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+{
+    // GoogleAPIClient to interface with the Location API
+    private GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
 
+    private GoogleMap mMap;
+
+    // ListView and Adapter for messages
     List<String> messageList = Lists.newArrayList();
-
     ArrayAdapter<String> adapter;
 
     @Override
@@ -37,6 +50,10 @@ public class MainActivity extends AppCompatActivity
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Set up the GoogleAPIClient
+        buildGoogleApiClient();
+        mGoogleApiClient.connect();
 
         // Set up the posts list
         adapter = new ArrayAdapter<>(this,
@@ -71,6 +88,15 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
+    // Setup the GoogleAPIClient
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -96,7 +122,35 @@ public class MainActivity extends AppCompatActivity
     // Callback triggers when the map is ready to use
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d("Layer7", "Map finished loading.");
-        googleMap.setMyLocationEnabled(true);
+        Log.d("Layer6debug", "Map finished loading.");
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            double lat = mLastLocation.getLatitude(),
+                    lng = mLastLocation.getLongitude();
+            Log.d("Layer6debug", "Lat:" + lat + ", Long:" + lng);
+            if (mMap != null) {
+                // Move map to user's location
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 17.0f));
+            }
+        } else {
+            Log.d("Layer6debug", "No last location found");
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("Layer6debug", "CONNECTION FAILED");
     }
 }
