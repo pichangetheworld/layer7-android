f64c20a69eec54b500c575df6b84e513b3896269
pchan <pichangetheworld@gmail.com>
2015-09-02 10:34:13 +0900

    something is broken, add TODO

--- app/src/main/java/layer7/layer7/MainActivity.java
+++ app/src/main/java/layer7/layer7/MainActivity.java
@@ -168,6 +168,8 @@ public class MainActivity extends AppCompatActivity
                 for (int i = 0; i < responses.length(); i++) {
                     try {
                         JSONObject message = (JSONObject) responses.get(i);
+
+                        // TODO Only add this message if it hasn't already been added
                         messageList.add(message.getString("body"));
                     } catch (JSONException e) {
                         e.printStackTrace();
