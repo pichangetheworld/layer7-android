06017b795ebe1199a7461a54f0dc0eafc5d9aa2b
P Chan <shiroyamaryu@gmail.com>
2015-09-09 10:26:41 +0900

    Integrated Fabric (Twitter Login for Android) into Layer7

--- app/src/main/java/layer7/layer7/LoginActivity.java
+++ app/src/main/java/layer7/layer7/LoginActivity.java
@@ -1,7 +1,17 @@
 package layer7.layer7;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * System Trump
@@ -11,9 +21,36 @@ import android.support.v7.app.AppCompatActivity;
  * All Rights Reserved
  */
 public class LoginActivity extends AppCompatActivity {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "qxXvRbw83sPMmd15nscoP6Sk3";
+    private static final String TWITTER_SECRET = "Azf30eak2FFCN2GbtKMfdJUqHAX3RQ5eKXw1Nv21MRpCO2sbae";
+
+    private TwitterLoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_login);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
