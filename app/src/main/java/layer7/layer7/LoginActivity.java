245b2f5ac145497a9ca3f949fa43e5436b70c2d6
P Chan <shiroyamaryu@gmail.com>
2015-09-09 10:07:48 +0900

    Adding LoginActivity and layout

---
+++ app/src/main/java/layer7/layer7/LoginActivity.java
@@ -0,0 +1,19 @@
+package layer7.layer7;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * System Trump
+ * <p>
+ * Created by P Chan on 9/9/15.
+ * Copyright Paper Fan Inc.
+ * All Rights Reserved
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
